a148ed0d4167ddf9580d34bd98ba91a1d1508b5b
Himabindu Poshala <himabindu.poshala23@gmail.com>
2019-02-07 15:29:59 -0600

    added helper methods in test

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,32 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Sum_WorksWithsumoftwonumbers()
         {
-
+            var expected = 10;
+            var a = 5;
+            var b = 5;
+            var actual = sum(a, b);
+            Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(2)]
+        [InlineData(6)]
+        [InlineData(8)]
+        public void Iseven_WorksWithManyInputs(int val)
+        {
+            Assert.True(Iseven(val));
+        }
+        int sum(int a, int b)
+        {
+            return a + b;
+        }
+        bool Iseven(int value)
+        {
+            return value % 2 == 0;
+        }
+
     }
 }
